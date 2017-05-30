54b48e6e7290d6d48649f8c8a0911155b12e62cc
Christopher Wilkinson <git@wilk.tech>
2017-05-30 09:31:10 +0100

    Make router properties customisable

--- src/TechWilk/Twig/Extension/HashtagifyUrlGenerator/SlimHashtagifyUrlGenerator.php
+++ src/TechWilk/Twig/Extension/HashtagifyUrlGenerator/SlimHashtagifyUrlGenerator.php
@@ -6,14 +6,18 @@ use TechWilk\Twig\Extension\HashtagifyUrlGeneratorInterface;
 class SlimHashtagifyUrlGenerator implements HashtagifyUrlGeneratorInterface
 {
   private $router;
+  private $routeName;
+  private $identifier;
 
-  public function __construct(\Slim\Router $router)
+  public function __construct(\Slim\Router $router, $routeName, $identifier)
   {
     $this->router = $router;
+    $this->routeName = $routeName;
+    $this->identifier = $identifier;
   }
 
   public function urlForHashtag($hashtag)
   {
-    return $this->router->pathFor('tag', [ 'tag' => $hashtag ] );
+    return $this->router->pathFor($this->routeName, [ $this->identifier => $hashtag ] );
   }
 }
