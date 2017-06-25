d8ba3b9a0b06e97f29043f901e49ac54c800b601
Christopher Wilkinson <git@wilk.tech>
2017-06-25 07:28:12 +0000

    Apply fixes from StyleCI

--- src/TechWilk/Twig/Extension/HashtagifyUrlGenerator/BasicHashtagifyUrlGenerator.php
+++ src/TechWilk/Twig/Extension/HashtagifyUrlGenerator/BasicHashtagifyUrlGenerator.php
@@ -1,19 +1,20 @@
 <?php
+
 namespace TechWilk\Twig\Extension\HashtagifyUrlGenerator;
 
 use TechWilk\Twig\Extension\HashtagifyUrlGeneratorInterface;
 
 class BasicHashtagifyUrlGenerator implements HashtagifyUrlGeneratorInterface
 {
-  private $urlBase;
+    private $urlBase;
 
-  public function __construct($urlBase)
-  {
-    $this->urlBase = $urlBase;
-  }
+    public function __construct($urlBase)
+    {
+        $this->urlBase = $urlBase;
+    }
 
-  public function urlForHashtag($hashtag)
-  {
-    return $this->urlBase . $hashtag;
-  }
+    public function urlForHashtag($hashtag)
+    {
+        return $this->urlBase.$hashtag;
+    }
 }
